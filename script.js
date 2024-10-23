5bdc2f8d523495b7c4d33e59767963224d2310ef
sandip-dabhi <sandipdabhi970@gmail.com>
2024-10-23 23:02:40 +0530

    project files

--- script.js
+++ script.js
@@ -20,58 +20,58 @@ let curr_track = document.createElement('audio');
 let track_list = [
     {
         name: "Abhi_Toh_Party_Shuru_Hui_Hai",
-        image: "m1.jpg",
-        path: "Abhi_Toh_Party_Shuru_Hui_Hai.mp3"
+        image: "src/img/m1.jpg",
+        path: "src/song/Abhi_Toh_Party_Shuru_Hui_Hai.mp3"
     },
     {
         name: "Aankh_Marey_",
-        image: "m2.jpg",
-        path: "Aankh_Marey___.mp3"
+        image: "src/img/m2.jpg",
+        path: "src/song/Aankh_Marey___.mp3"
     },
     {
         name: "Akh_Lad_Jaave_Loveyatri_",
-        image: "m3.jpg",
-        path: "Akh_Lad_Jaave___Loveyatri___.mp3"
+        image: "src/img/m3.jpg",
+        path: "src/song/Akh_Lad_Jaave___Loveyatri___.mp3"
     },
     {
         name: "Bom_Diggy_Diggy",
-        image: "m4.jpg",
-        path: "Bom_Diggy_Diggy.mp3"
+        image: "src/img/m4.jpg",
+        path: "src/song/Bom_Diggy_Diggy.mp3"
     },
     {
         name: "Chalti_Hai_Kya_9_Se_12_",
-        image: "m5.jpg",
-        path: "Chalti_Hai_Kya_9_Se_12_.mp3"
+        image: "src/img/m5.jpg",
+        path: "src/song/Chalti_Hai_Kya_9_Se_12_.mp3"
     },
     {
         name: "Dheere_Dheere_Se_Meri_Zindagi_",
-        image: "m6.jpg",
-        path: "Dheere_Dheere_Se_Meri_Zindagi_.mp3"
+        image: "src/img/m6.jpg",
+        path: "src/song/Dheere_Dheere_Se_Meri_Zindagi_.mp3"
     },
     {
         name: "Dhinka_Chika_",
-        image: "m7.jpg",
-        path: "Dhinka_Chika__.mp3"
+        image: "src/img/m7.jpg",
+        path: "src/song/Dhinka_Chika__.mp3"
     },
     {
         name: "Ek_Do_Teen",
-        image: "m8.jpg",
-        path: "Ek_Do_Teen.mp3"
+        image: "src/img/m8.jpg",
+        path: "src/song/Ek_Do_Teen.mp3"
     },
     {
         name: "KGF__Gali_Gali_",
-        image: "m9.jpg",
-        path: "KGF__Gali_Gali_.mp3"
+        image: "src/img/m9.jpg",
+        path: "src/song/KGF__Gali_Gali_.mp3"
     },
     {
         name: "Luka_Chuppi_COCA_COLA_",
-        image: "m10.jpg",
-        path: "Luka_Chuppi__COCA_COLA_.mp3"
+        image: "src/img/m10.jpg",
+        path: "src/song/Luka_Chuppi__COCA_COLA_.mp3"
     },
     {
         name: "Lungi_Dance_The_",
-        image: "m11.jpg",
-        path: "Lungi_Dance__The_.mp3"
+        image: "src/img/m11.jpg",
+        path: "src/song/Lungi_Dance__The_.mp3"
     }
 ];
 
